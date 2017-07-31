0a9c94b6e8029ccc7300f5a980105efb634d96e5
lujanfernaud <lujanfernaud@gmail.com>
2017-07-31 16:32:54 +0100

    Refactor to remove global variable

--- app.rb
+++ app.rb
@@ -3,22 +3,22 @@ require "sinatra/reloader" if development?
 
 require "./lib/hangman"
 
-@@hangman = Hangman.new
+set :hangman, Hangman.new
 
 get "/" do
-  guess = params["guess"] || ""
+  guess   = params["guess"] || ""
   restart = params["restart"]
 
-  @@hangman = Hangman.new if restart
+  settings.hangman = Hangman.new if restart
 
-  @@hangman.check(guess)
+  settings.hangman.check(guess)
 
-  secret_word      = @@hangman.secret_word.join
-  hidden_word      = @@hangman.hidden_word.join(" ")
-  wrong_characters = @@hangman.wrong_characters
-  guesses_left     = @@hangman.guesses_left
-  game_finished    = @@hangman.game_finished
-  game_result      = @@hangman.game_result
+  secret_word      = settings.hangman.secret_word.join
+  hidden_word      = settings.hangman.hidden_word.join(" ")
+  wrong_characters = settings.hangman.wrong_characters
+  guesses_left     = settings.hangman.guesses_left
+  game_finished    = settings.hangman.game_finished
+  game_result      = settings.hangman.game_result
 
   erb :index, locals: { secret_word:      secret_word,
                         hidden_word:      hidden_word,
