f2bc94753b4e6cbea8afc9af4ddb067aeab809b7
lujanfernaud <lujanfernaud@gmail.com>
2017-08-01 21:41:07 +0100

    Use instance variables instead of locals

--- app.rb
+++ app.rb
@@ -17,17 +17,12 @@ get "/" do
 
   settings.hangman.check(guess)
 
-  secret_word      = settings.hangman.secret_word.join
-  hidden_word      = settings.hangman.hidden_word.join(" ")
-  wrong_characters = settings.hangman.wrong_characters.join(" ")
-  guesses_left     = settings.hangman.guesses_left
-  game_finished    = settings.hangman.game_finished
-  game_result      = settings.hangman.game_result
+  @secret_word      = settings.hangman.secret_word.join
+  @hidden_word      = settings.hangman.hidden_word.join(" ")
+  @wrong_characters = settings.hangman.wrong_characters.join(" ")
+  @guesses_left     = settings.hangman.guesses_left
+  @game_finished    = settings.hangman.game_finished
+  @game_result      = settings.hangman.game_result
 
-  erb :index, locals: { secret_word:      secret_word,
-                        hidden_word:      hidden_word,
-                        wrong_characters: wrong_characters,
-                        guesses_left:     guesses_left,
-                        game_finished:    game_finished,
-                        game_result:      game_result }
+  erb :index
 end
